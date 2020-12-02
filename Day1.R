ddc3243e19dc0df015126de491b0210aa5f15eb6
Louis-dM <Louis-dM@users.noreply.github.com>
2020-12-02 11:20:14 +0000

    Update Day1.R
    added some comments and decided to use functions instead to exit loops earlier

--- Day1.R
+++ Day1.R
@@ -1,20 +1,31 @@
+setwd("~/Other/Programming/Advent of code/2020/Data")
 expenses <- scan("Day1data.txt")
 n <- length(expenses)
 
-for (i in 1:(n-1)) {
-  for (j in (i+1):n) {
-    if (expenses[i] + expenses[j] == 2020) {
-      print(expenses[i] * expenses[j])
-    }
-  }
-}
 
-for (i in 1:(n-2)) {
-  for (j in (i+1):(n-1)) {
-    for (k in (j+1):n) {
-      if (expenses[i] + expenses[j] + expenses[k] == 2020) {
-        print(expenses[i] * expenses[j] * expenses[k])
+### PART 1 ###
+part1 <- function() {
+  for (i in 1:(n-1)) {
+    for (j in (i+1):n) {
+      if (expenses[i] + expenses[j] == 2020) {
+        return(expenses[i] * expenses[j])
       }
     }
   }
 }
+part1()
+
+
+### PART 2 ###
+part2 <- function() {
+  for (i in 1:(n-2)) {
+    for (j in (i+1):(n-1)) {
+      for (k in (j+1):n) {
+        if (expenses[i] + expenses[j] + expenses[k] == 2020) {
+          return(expenses[i] * expenses[j] * expenses[k])
+        }
+      }
+    }
+  }
+}
+part2()
